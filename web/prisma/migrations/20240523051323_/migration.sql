857649b01da3f9244415d6020b91f10ea03560fb
8dfa4166 <8dfa4166@gmail.com>
2024-05-23 18:22:56 +0900

    deleted middle name

---
+++ web/prisma/migrations/20240523051323_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `middleName` on the `IndividualNumber` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "IndividualNumber" DROP COLUMN "middleName";
